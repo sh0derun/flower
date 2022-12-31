9a4286ed413794fb5d59a70dd4c1b0789260c5be
Sh@drun <5242001+sh0derun@users.noreply.github.com>
2023-01-01 00:05:11 +0100

    reading code from provided input file name

--- src/main/java/org/lang/flower/Main.java
+++ src/main/java/org/lang/flower/Main.java
@@ -2,13 +2,31 @@ package org.lang.flower;
 
 import org.lang.flower.ast.AstFunctionCall;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Optional;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 
 public class Main {
     public static void main(String[] args) {
-        Parser parser = new Parser("print(\"hello hhh\")");
+        if (args.length < 1) {
+            throw new IllegalArgumentException("\nflower: fatal error: no input file");
+        }
+
+        String inputFile = args[0];
+        Optional<String> code = Optional.empty();
+
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(inputFile))) {
+            code = bufferedReader.lines().reduce((s, s2) -> s +"\n"+ s2);
+        } catch (IOException e) {
+            throw new IllegalArgumentException("\nflower: error: " + inputFile + ": No such file\nflower: fatal error: no input file");
+        }
+
+        Parser parser = new Parser(code.orElseThrow());
         AstFunctionCall<Object> ast = parser.parseFunctionCall();
         System.out.println(ast);
 
@@ -16,8 +34,6 @@ public class Main {
         System.out.println(asm);
 
         String assemblyFileName = "code";
-
-
 
         try (FileWriter fileWriter = new FileWriter(assemblyFileName+".asm")) {
             fileWriter.append(asm);
